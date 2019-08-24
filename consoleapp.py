cdfe3f97fee4efc3735b76deb5613b13e41f77a7
Eric Dennison <ericd@netdenizen.com>
2019-08-24 10:43:45 -0400

    Updated from Brython Server: 8/24/2019 10:42:43 AM

--- consoleapp.py
+++ consoleapp.py
@@ -1,3 +1,15 @@
 """ ConsoleApp sub-class of ggame App """
 
 from ggame import App
+
+def gginput(prompt):
+    return input(prompt)
+
+def step():
+    """ periodic processing """
+    
+    x = gginput("enter x")
+    y = gginput("enter y")
+    print(x, y)
+
+App().run(step)
