a7d0e6a6b49c71cdb9780aa508c3e566435b430c
Eric Dennison <ericd@netdenizen.com>
2019-08-24 11:53:47 -0400

    Updated from Brython Server: 8/24/2019 11:52:45 AM

--- consoleapp.py
+++ consoleapp.py
@@ -7,11 +7,11 @@ class ConsoleApp(App):
     _input = input
     _instack = []
     
-    def __init__(self, *args, entry=None):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args)
         self._instack = []
         self.run()
-        self._entry = entry
+        self._entry = kwargs.get('entry', None)
         self._routemain(None)
     
     def _routemain(self, newstate, data=None):
