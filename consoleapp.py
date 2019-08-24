395ed739fa717e8b854ec2e6311cda819dc0c7cb
Eric Dennison <ericd@netdenizen.com>
2019-08-24 11:26:13 -0400

    Updated from Brython Server: 8/24/2019 11:25:10 AM

--- consoleapp.py
+++ consoleapp.py
@@ -2,31 +2,55 @@
 
 from ggame import App
 
-def gginput(prompt):
-    return input(prompt)
+   
 
 
-def main():
+def main(state, data):
     """ main function """
-    x = gginput("enter x")
-    y = gginput("enter y")
-    print(x, y)
+    global x
+    global y
+    if not state:
+        input("enter x", 1)
+    elif state == 1:
+        x = data
+        input("enter y", 2)
+    else:
+        y = data
+        print(x, y)
     
 
 class ConsoleApp(App):
-
-
+    """ ConsoleApp sub-class of App """
+    _input = input
+    
     def __init__(self, *args, entry=None):
         super().__init__(*args)
+        self._instack = []
         self.run()
-        if not entry:
-            main()
+        self._entry = entry
+        self._routemain(None)
+    
+    def _routemain(self, newstate):
+        if not self._entry:
+            main(newstate)
         else:
-            entry()
+            self._entry(newstate)
+        
+    def _pushInput(self, prompt, newstate, newfunc):
+        self._instack.append((prompt, newstate, newfunc))
         
     def step(self):
         """ periodic processing """
-        pass
-
+        if self._instack:
+            prompt, newstate, newfunc = self._instack.pop(0)
+            inval = self._input(prompt)
+            if newfunc:
+                newfunc(newstate)
+            else:
+                self._routemain(newstate)
+            
+def input(prompt, newstate=None, newfunc=None):
+    inval = _input(prompt)
+ 
 
 ConsoleApp()
